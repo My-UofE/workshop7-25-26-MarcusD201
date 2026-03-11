8335773467805f95cb18233cfd2c100e0b735c61
MarcusD201 <mlmd201@exeter.ac.uk>
2026-03-11 17:57:02 +0000

    ...

--- Rectangle.java
+++ Rectangle.java
@@ -4,12 +4,45 @@ double width;
 
 double height;
 
+public Rectangle(){
 
+width=1;
 
+height=1;
 
+}
 
+public Rectangle( double newWidth , double newHeight, String newcolour ){
 
+width = newWidth;
 
+height= newHeight;
+
+ Shape.setColor(newcoulor);
+}     
+
+public int getWidth(){
+    
+return width;
+
+    }
+
+public int getHeight(){
+
+    return height;
+}
+
+public double area(){
+
+return height * width;
+
+}
+
+public double perimeter () {
+
+return (2*width)+(2*height);
+
+}
 
 
 
