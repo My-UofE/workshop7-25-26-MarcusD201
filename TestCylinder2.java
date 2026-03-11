ff54d330f22467746c324f5f9f365e9dab5ea94c
MarcusD201 <mlmd201@exeter.ac.uk>
2026-03-11 11:46:26 +0000

    ...

--- TestCylinder2.java
+++ TestCylinder2.java
@@ -11,6 +11,24 @@ public class TestCylinder2 {
        double v = ((Cylinder)cy2).getVolume();
 
        System.out.printf("Height: %.2f, Radius: %.2f, Area: %.2f, Volume: %.2f\n", h, r, a, v);
+ System.out.println("\nrunning\nCircle c3 = new Circle();\nc3.printClassInfo();");
+    Circle c3 = new Circle();
+    c3.printClassInfo();
+    
+    System.out.println("\nrunning\nCylinder cy4 = new Cylinder();\ncy4.printClassInfo();");
+    Cylinder cy4 = new Cylinder();
+    cy4.printClassInfo();
+    
+    System.out.println("\nrunning\nCircle cy5 = new Cylinder();\ncy5.printClassInfo();");
+    Circle cy5 = new Cylinder(); // upcasting
+    cy5.printClassInfo();
+    
+    // this will give a compiler warning; but can compile and run
+    System.out.println("\nrunning\ncy4.printClassInfoStatic();");
+    cy4.printClassInfoStatic();
 
+    // this will give a compiler warning; but can compile and run
+    System.out.println("\nrunning\ncy5.printClassInfoStatic();");
+    cy5.printClassInfoStatic();
     }
 }
